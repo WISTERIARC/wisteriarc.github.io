8b2859bd5820bda105823d94123af7b883c3adf5
S16-Mk <shinichiro.makino@wisteriarc.com>
2025-02-06 22:56:56 +0900

    影響のなさそうなところ

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'WISTERIARC';
+export const SITE_TAGLINE = 'Developer Blog & Portfolio Website'; // site tagline
+export const SITE_DESCRIPTION = 'Developer Blog & Portfolio Website'; // site meta description for SEO
